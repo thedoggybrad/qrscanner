7569f46d7d97e2fdc606ccfe718bad2780fd8c99
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-23 18:08:20 +0800

    Unfinished PWA Service Worker

---
+++ pwa/service_worker_main.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const CACHE_NAME = 'cache-v2';
+// The files we want to cache
+const resourceList = [
+  '/',
+  'https://thedoggybrad.github.io/qrscanner/index.html',
+
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => {
+    return cache.addAll(resourceList);
+  }));
+});
+
+function addToCache(cacheName, resourceList) {
+  caches.open(cacheName).then(cache => {
+    return cache.addAll(resourceList);
+  });
+}
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => {
+    return response || fetch(event.request);
+  }));
+});
