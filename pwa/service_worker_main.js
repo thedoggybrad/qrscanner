045492a991399a060288979ea29e42c36a89b172
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-24 11:53:39 +0800

    Update service_worker_main.js

--- pwa/service_worker_main.js
+++ pwa/service_worker_main.js
@@ -25,6 +25,7 @@ const resourceList = [
   'https://thedoggybrad.github.io/qrscanner/images/screenshots/screenshot3.png',
   'https://www.w3.org/2000/svg',
   'http://www.w3.org/1999/xlink'
+
 ];
 
 self.addEventListener('install', event => {
